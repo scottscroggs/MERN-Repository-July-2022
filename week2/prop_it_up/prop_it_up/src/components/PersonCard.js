41db0fcff3c503daf5b504f09bad2cf2e8a871d6
scottscroggs <98123206+scottscroggs@users.noreply.github.com>
2022-07-12 13:18:55 -0500

    updating Prop it Up to include Putting It Together

--- week2/prop_it_up/prop_it_up/src/components/PersonCard.js
+++ week2/prop_it_up/prop_it_up/src/components/PersonCard.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
     // Deconstructing the props into variables
-    const {firstName, lastName, hairColor} = props;
+    const {firstName, lastName, age, hairColor} = props;
+
+    //Creating a useState that stores the users current age.
+    const [stateAge, setStateAge] = useState(age);
+
+    //Function to increment the age stored in State.
+    const handleClick = () => {
+        setStateAge(
+            stateAge + 1
+        )
+    }
 
     //The component being returned
     return( 
         <div>
             <h1 style={{color:"black"}}>{firstName} {lastName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {stateAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={handleClick}>Birthday Button for {firstName} {lastName}</button>
         </div>
+
+        
     );
 }
 export default PersonCard;
